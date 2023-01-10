c8a1481299f69cd5f2088fa6cfc86f71f288cfe3
Galen Sho Sato <galen.sato@gmail.com>
2023-01-10 13:37:25 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+    return Math.abs(42 - someValue)
+}
+
+function distanceFromHqInFeet(someValue) {
+    return distanceFromHqInBlocks(someValue) * 264
+}
+
+
+function distanceTravelledInFeet(start, destination){
+    return Math.abs((start - destination) * 264)
+    
+}
+
+function calculatesFarePrice(start, destination) {
+    let x
+    let y
+    if (distanceTravelledInFeet(start, destination) <= 400) {
+        x = 0
+        return x
+    }
+
+    else if (distanceTravelledInFeet(start, destination) > 400 && distanceTravelledInFeet(start, destination) <= 2000) {
+        y = distanceTravelledInFeet(start, destination)
+        x = (y - 400) * .02
+        return x
+    }
+
+    else if (distanceTravelledInFeet(start, destination) > 2000 && distanceTravelledInFeet(start, destination) <= 2500) {
+        x = 25
+        return x
+    }
+
+    else if (distanceTravelledInFeet(start, destination) > 2500) {
+        x = 'cannot travel that far'
+        return x
+    }
+}
+
